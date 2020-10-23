b65a8f212d8263302cb1de6944b48d0824bc649e
Malcolm_Leake <echelon1089@gmail.com>
2020-10-23 09:02:07 -0400

    All routes are created properly

---
+++ realestateapp/src/components/cashoffer.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class CashOffer extends Component {
+    render(){
+        return(
+            <div><h1> Get A Cash Offer For Your House Today!</h1></div>
+        )
+    }
+}
+
+export default CashOffer
---
+++ realestateapp/src/components/listings.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+
+
+
+class Listings extends Component {
+    render(){
+        return(
+            <div>Investor Listings</div>
+        )
+    }
+}
+
+export default Listings
