a3fef7c350904de09fad2279ab8f665f76089c3f
Malcolm_Leake <echelon1089@gmail.com>
2020-12-17 21:51:41 -0500

    updating favicon

--- realestateapp/src/components/cashoffer.js
+++ realestateapp/src/components/cashoffer.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class CashOffer extends Component {
     render(){
         return(
-            <div><h1> Get A Cash Offer For Your House Today!</h1></div>
+            <div><h1> Get Multiple Offers For Your House Today!</h1></div>
         )
     }
 }
